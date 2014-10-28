e64003da8fad590b6154c8719ff91700b2a3b02a
Slaviana Kaziuko <s.kazuko@investars.com>
2014-10-28 10:28:21 +0300

    add db for 2012

---
+++ AUTOPARKDB/Schema Objects/Schemas/dbo/Tables/Личный состав.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Личный состав]
+(
+	[табельный  номер] [int] NOT NULL CONSTRAINT [PK_личный состав] PRIMARY KEY ([табельный  номер]),
+	[ФИО] [nvarchar](50) NOT NULL,
+	[серия водительского удостоверения] [nvarchar](50) NOT NULL,
+	[номер водительского удостоверения] [int] NOT NULL,
+	[классность] [int] NOT NULL,
+	[срок действия мед. справки] [date] NOT NULL,
+)
---
+++ AUTOPARKDB/Schema Objects/Schemas/dbo/Tables/Подвижной состав.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Подвижной состав]
+(
+	[ID] [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_Подвижной состав] PRIMARY KEY ([ID]),
+	[Марка и модель] [nvarchar](50) NOT NULL,
+	[Гос.номер] [nvarchar](50) NOT NULL,
+	[Гаражный номер] [nvarchar](50) NULL,
+	[Тип двигателя] [nvarchar](50) NULL,
+	[Год выпуска] [date] NULL,
+	[Инвентарный номер] [nvarchar](50) NULL,
+	[Вид топлива] [nvarchar](50) NULL,
+	[дата окончания страховки] [date] NULL,
+)
