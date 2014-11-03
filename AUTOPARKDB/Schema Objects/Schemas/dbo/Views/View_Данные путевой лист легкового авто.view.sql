3a69c37343f86278f07bed12a50697adab0dcb44
SlavianaKaziuko <s.kazuko@investars.com>
2014-11-03 09:38:50 +0300

    db changes

--- AUTOPARKDB/Schema Objects/Schemas/dbo/Views/View_Данные путевой лист легкового авто.view.sql
+++ AUTOPARKDB/Schema Objects/Schemas/dbo/Views/View_Данные путевой лист легкового авто.view.sql
@@ -1,22 +1,130 @@
 ﻿CREATE VIEW [dbo].[View_Данные путевой лист легкового авто]
-AS SELECT     [Число], 
-			[Время выезда] AS Время_выезда, 
-			[Время возвращения] AS Время_возвращения, 
-			[Время в наряде] AS Время_в_наряде, 
-            [Показания спидометра при выезде] AS Показания_спидометра_при_выезде, 
-            [Показания спидометра при возвращении] AS Показания_спидометра_при_возращении,
-			[Пробег км] AS Пробег_км, 
-            [Остаток топлива при выезде] AS Остаток_топлива_при_выезде, 
-			[Остаток топлива при возвращении] AS Остаток_топлива_при_возвращении, 
-            [Заправлено топлива л] AS Заправлено_топливо_л, 
-			[Дата и № чека АЗС] AS Дата_и_номер_чека_АЗС, 
-            [Расход топлива по норме] AS Расход_топлива_по_норме, 
-			[Расход топлива фактически] AS Расход_топлива_фактически, 
-            [Расход топлива экономия перерасход] AS Расход_топлива_экономия_перерасход, 
-            '' AS Автомобиль_технически_исправлен_выезд_разрешен_подпись_штамп_уполномоченного_лица,
-            '' AS Автомобиль_технически_исправлен_прнял_подпись_водителя, 
-            '' AS При_возвращении_автомобиль_сдал_подпись_водителя, 
-            '' AS При_возвращении_автомобиль_принял_подпись_штамп_уполномоченного_лица,
-            '' AS Водитель_по_состоянию_здоровья_к_управлению_допущен_подпись_штамп_уполномоченного_лица,
-            '' AS Подпись_штамп_лица_ответственного_за_оформление_путевого_листа
+AS
+SELECT     Число, [Время выезда] AS Время_выезда, [Время возвращения] AS Время_возвращения, [Время в наряде] AS Время_в_наряде, 
+                      [Показания спидометра при выезде] AS Показания_спидометра_при_выезде, 
+                      [Показания спидометра при возвращении] AS Показания_спидометра_при_возращении, [Пробег км] AS Пробег_км, 
+                      [Остаток топлива при выезде] AS Остаток_топлива_при_выезде, [Остаток топлива при возвращении] AS Остаток_топлива_при_возвращении, 
+                      [Заправлено топлива л] AS Заправлено_топливо_л, [Дата и № чека АЗС] AS Дата_и_номер_чека_АЗС, 
+                      [Расход топлива по норме] AS Расход_топлива_по_норме, [Расход топлива фактически] AS Расход_топлива_фактически, 
+                      [Автомобиль технически исправлен выезд разрешен (подрись штамп уполномоченного лица)] AS Автомобиль_технически_исправлен_выезд_разрешен_подпись_штамп_уполномоченного_лица,
+                       [Расход топлива экономия перерасход] AS Расход_топлива_экономия_перерасход, 
+                      [Автомобиль технически исправлен принял(подпись водителя)] AS Автомобиль_технически_исправлен_прнял_подпись_водителя, 
+                      [При возвращении автомобиль сдал(подпись водителя)] AS При_возвращении_автомобиль_сдал_подпись_водителя, 
+                      [При возвращении автомобиль принял(подпись штамп уполномоченного лица)] AS При_возвращении_автомобиль_принял_подпись_штамп_уполномоченного_лица,
+                       [Водитель по состоянию здоровья к управлению допущен (подпись штам уполномоченного лица)] AS Водитель_по_состоянию_здоровья_к_управлению_допущен_подпись_штамп_уполномоченного_лица,
+                       [Подпись(штамп) лица,ответственного за оформление путевого листа] AS Подпись_штамп_лица_ответственного_за_оформление_путевого_листа
 FROM         dbo.[Данные Путевой лист легкового авто]
+
+
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Данные Путевой лист легкового авто"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 125
+               Right = 625
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 7530
+         Alias = 7545
+         Table = 3255
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'View_Данные путевой лист легкового авто'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'View_Данные путевой лист легкового авто'
+
---
+++ AUTOPARKDB/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,178 @@
+﻿
+USE [master]
+GO
+/****** Object:  Database [Путевые листы]    Script Date: 11/02/2014 17:57:35 ******/
+CREATE DATABASE [Путевые листы] ON  PRIMARY 
+( NAME = N'Путевые листы', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\Путевые листы.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'Путевые листы_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\Путевые листы_1.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+ALTER DATABASE [Путевые листы] SET COMPATIBILITY_LEVEL = 100
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Путевые листы].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [Путевые листы] SET ANSI_NULL_DEFAULT ON
+GO
+ALTER DATABASE [Путевые листы] SET ANSI_NULLS ON
+GO
+ALTER DATABASE [Путевые листы] SET ANSI_PADDING ON
+GO
+ALTER DATABASE [Путевые листы] SET ANSI_WARNINGS ON
+GO
+ALTER DATABASE [Путевые листы] SET ARITHABORT ON
+GO
+ALTER DATABASE [Путевые листы] SET AUTO_CLOSE OFF
+GO
+ALTER DATABASE [Путевые листы] SET AUTO_CREATE_STATISTICS ON
+GO
+ALTER DATABASE [Путевые листы] SET AUTO_SHRINK OFF
+GO
+ALTER DATABASE [Путевые листы] SET AUTO_UPDATE_STATISTICS ON
+GO
+ALTER DATABASE [Путевые листы] SET CURSOR_CLOSE_ON_COMMIT OFF
+GO
+ALTER DATABASE [Путевые листы] SET CURSOR_DEFAULT  LOCAL
+GO
+ALTER DATABASE [Путевые листы] SET CONCAT_NULL_YIELDS_NULL ON
+GO
+ALTER DATABASE [Путевые листы] SET NUMERIC_ROUNDABORT OFF
+GO
+ALTER DATABASE [Путевые листы] SET QUOTED_IDENTIFIER ON
+GO
+ALTER DATABASE [Путевые листы] SET RECURSIVE_TRIGGERS OFF
+GO
+ALTER DATABASE [Путевые листы] SET  DISABLE_BROKER
+GO
+ALTER DATABASE [Путевые листы] SET AUTO_UPDATE_STATISTICS_ASYNC OFF
+GO
+ALTER DATABASE [Путевые листы] SET DATE_CORRELATION_OPTIMIZATION OFF
+GO
+ALTER DATABASE [Путевые листы] SET TRUSTWORTHY OFF
+GO
+ALTER DATABASE [Путевые листы] SET ALLOW_SNAPSHOT_ISOLATION OFF
+GO
+ALTER DATABASE [Путевые листы] SET PARAMETERIZATION SIMPLE
+GO
+ALTER DATABASE [Путевые листы] SET READ_COMMITTED_SNAPSHOT OFF
+GO
+ALTER DATABASE [Путевые листы] SET HONOR_BROKER_PRIORITY OFF
+GO
+ALTER DATABASE [Путевые листы] SET RECOVERY FULL
+GO
+ALTER DATABASE [Путевые листы] SET  MULTI_USER
+GO
+ALTER DATABASE [Путевые листы] SET PAGE_VERIFY NONE
+GO
+ALTER DATABASE [Путевые листы] SET DB_CHAINING OFF
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'Путевые листы', N'ON'
+GO
+USE [Путевые листы]
+GO
+/****** Object:  Table [dbo].[Водители авто]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Данные Путевой лист легкового авто]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Движение ТСМ]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Задание водителю]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Заправка ТСМ]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Личный состав]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Нормы расхода ГСМ на авто,тракторы]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Подвижной состав]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Table [dbo].[Путевой лист Грузового авто]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Путевой лист легкового автомобиля]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  Table [dbo].[Работа водителя и автомобиля]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Данные путевой лист легкового авто]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Движение  ТСМ]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Задание водителю]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Заправка ТСМ]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Личный состав]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Подвижной состав]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Работа водителя и авто]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  View [dbo].[View_Работа водителя и автомобиля]    Script Date: 11/02/2014 17:57:35 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+USE [master]
+GO
+ALTER DATABASE [Путевые листы] SET  READ_WRITE
+
+GO
