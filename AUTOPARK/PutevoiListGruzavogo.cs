e27f87cd74d118820ee44b52acbb61800fb6ba16
SashaGradova <natahagradova@mail.ru>
2014-11-13 15:53:51 +0300

    добавление в путевой лист грузового авто

--- AUTOPARK/PutevoiListGruzavogo.cs
+++ AUTOPARK/PutevoiListGruzavogo.cs
@@ -5,6 +5,19 @@ namespace AUTOPARK
 {
     public partial class PutevoiListGruzavogo : Form
     {
+        private readonly BindingSource _bindingAuto = new BindingSource();
+        private readonly BindingSource _bidingVoditel = new BindingSource();
+       private readonly BindingSource _bindingDannie = new BindingSource();
+        private readonly string _number;
+        private readonly int _idauto;
+        private readonly int _idvod;
+        private readonly DateTime _dateStart;
+        private readonly DateTime _dateEnd;
+        private readonly bool _modeIsNew; ////true - add, false - update
+        private int PutevoiId { get; set; }
+
+
+
         public PutevoiListGruzavogo()
         {
             InitializeComponent();
@@ -13,17 +26,89 @@ namespace AUTOPARK
         public PutevoiListGruzavogo(int id)
         {
             InitializeComponent();
+
+            var tablep = new AutoparkDBTableAdapters.PodvijnoiTableAdapter();
+            _bindingAuto.DataSource = tablep.GetData();
+            if (_bindingAuto.Count == 0)
+            {
+                throw new Exception("Пожалуйста, заполните справочник \"Подвижной состав\"");
+            }
+            cbZnak.DataSource = _bindingAuto;
+            cbZnak.DisplayMember = "Гос_номер";
+            cbZnak.ValueMember = "ID";
+
+
+            var tablel = new AutoparkDBTableAdapters.LichniiTableAdapter();
+            _bidingVoditel.DataSource = tablel.GetData();
+            if (_bidingVoditel.Count == 0)
+            {
+                throw new Exception("Пожалуйста, заполните справочник \"Личный состав\"");
+            }
+            cbTabelniiNomer.DataSource = _bidingVoditel;
+            cbTabelniiNomer.DisplayMember = "табельный_номер";
+            cbTabelniiNomer.ValueMember = "табельный_номер";
+           var tablePutevoi = new AutoparkDBTableAdapters.PutevieGruzovieTableAdapter();
+             //var newLegkNumber = tablePutevoi.GetNewLegkNumber();
+            //if (newLegkNumber != null)
+            //    txtNumber.Text = newLegkNumber.Value.ToString(CultureInfo.InvariantCulture);
+            _dateStart = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            _dateEnd = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddDays(-1);
         }
 
-        private void PutevoiListGruzavogo_Load(object sender, EventArgs e)
+       
+
+         public PutevoiListGruzavogo(int id)
+        {
+            InitializeComponent();
+
+             _modeIsNew=false;
+             PutevoiId=id;
+             var tablep = new AutoparkDBTableAdapters.PodvijnoiTableAdapter();
+             _bindingAuto.DataSource = tablep.GetData();
+             if (_bindingAuto.Count ==0)
+             {
+             throw new Exception("Пожалуйста, заполните справочник \"Подвижной состав\"");
+             }
+             cbZnak.DataSource = _bindingAuto;
+             cbZnak.DisplayMember = "Гос_номер";
+             cbZnak.ValueMember = "ID";
+
+              var tablel = new AutoparkDBTableAdapters.LichniiTableAdapter();
+            _bidingVoditel.DataSource = tablel.GetData();
+            if (_bidingVoditel.Count == 0)
+            {
+                throw new Exception("Пожалуйста, заполните справочник \"Личный состав\"");
+            }
+            cbTabelniiNomer.DataSource = _bidingVoditel;
+            cbTabelniiNomer.DisplayMember = "табельный_номер";
+            cbTabelniiNomer.ValueMember = "табельный_номер";
+
+             var tableDannie = new AutoparkDBTableAdapters.PutevieGruzovieTableAdapter
+            _bindingDannie.DataSource = tableDannie.GetData();
+            _bindingDannie.Filter = "[ID_Путевого листа] = " + PutevoiId;
+            dgvPutevieLegkovie.DataSource = _bindingDannie;
+
+        }
+
+
+
+
+
+
+
+
+
+ }
+
+       private void PutevoiListGruzavogo_Load(object sender, EventArgs e)
         {
         }
 
-        private void txtMarka_TextChanged(object sender, EventArgs e)
-        {
+      private void txtMarka_TextChanged(object sender, EventArgs e)
+       {
         }
 
-        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+       private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
         }
 
@@ -44,7 +129,7 @@ namespace AUTOPARK
         {
             var f1 = new OtchetGruzavogo();
             f1.Show();
-        }
+        } 
 
         private void tsmiToMenu_Click(object sender, EventArgs e)
         {
