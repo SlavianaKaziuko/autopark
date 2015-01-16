ca4bab90925338fdcb18e1ec03ebcb14e1bdf364
SashaGradova <natahagradova@mail.ru>
2015-01-16 15:31:27 +0300

    Правильный перенос данных в грузом путевом

--- AUTOPARKDB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetToplivoGruz.proc.sql
+++ AUTOPARKDB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetToplivoGruz.proc.sql
@@ -10,8 +10,8 @@ BEGIN
 SELECT TOP (1) @result1 = ISNULL([Остаток_ТСМ1_возвр], 0), @result2 = ISNULL([Остаток_ТСМ2_возвр], 0) 
 FROM [Путевой лист Грузового авто]
 WHERE [ID_Автомобиля] = @Auto
-	AND [Дата_Время_возвращения_граф] <= @DateAt
-ORDER BY [Дата_Время_возвращения_граф] DESC
+	AND [Дата_Время_возвращения_факт] <= @DateAt
+ORDER BY [Дата_Время_возвращения_факт] DESC
 
 IF (@ToplivoID = 1)
 	SELECT ISNULL(@result1, 0) AS  N'Остаток_ТСМ_возвр'
